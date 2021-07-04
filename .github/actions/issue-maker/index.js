2b25cf7c6c76fc541827add3725ab4957fbe0b66
Taras S <tarassuhovenko@gmail.com>
2021-07-04 12:27:35 +0300

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -2,22 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  try {
-    const issueTitle = core.getInput("issue-title");
-    const jokeBody = core.getInput("joke");
-    const token = core.getInput("repo-token");
+    try {
+        const issueTitle = core.getInput("issue-title");
+        const jokeBody = core.getInput("joke");
+        const token = core.getInput("repo-token");
 
-    const octokit = new github.getOctokit(token);
+        const octokit = github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
-    });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        console.log(octokit)
+
+        const newIssue = await octokit.rest.issues.create({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            title: issueTitle,
+            body: jokeBody
+        });
+    } catch (err) {
+        core.setFailed(err.message);
+    }
 }
 
-run();
+run()
